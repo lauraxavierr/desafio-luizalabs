525fbe90d5103806efccab6940ff4cba9820cdaf
lauraxavierr <laurausual@gmail.com>
2021-11-11 22:56:03 -0300

    fix: alterando estrutura dos arquivos.

---
+++ config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'fanny.db.elephantsql.com',
+    username: 'ljlncxjj',
+    password: 'zARk807PFGDZiayytnzaiQmwfa-03KFZ',
+    database: 'ljlncxjj',
+    define: {
+	timeStamps: true,
+	underscored: true,
+	underscoredAll: true
+    }
+
+}
