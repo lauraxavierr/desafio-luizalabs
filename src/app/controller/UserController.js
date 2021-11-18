80d9b870d5a1957eaaa2c52c1d6a908be1c94940
lauraxavierr <laurausual@gmail.com>
2021-11-18 15:29:15 -0300

    feat: refatorando cadastro de usuários.

--- src/app/controller/UserController.js
+++ src/app/controller/UserController.js
@@ -8,15 +8,15 @@ class UserController {
 	async store(req, res) {
 		const schema = Yup.object().shape({
 			email: Yup.string().email().required(),
-			senha: Yup.string().required().min(6),
-			cep: Yup.string().required().min(8)
-		});
+			senha: Yup.string().required().min(6)
+		}); 
+		
 
 		if (!(await schema.isValid(req.body))) {
 			return res.status(401).json({
 				message: 'Dados inválidos'
 			});
-		}
+		} // Valida os dados passados no body da requisição.
 
 		const userExists = await User.findOne({
 			where: {
@@ -28,75 +28,23 @@ class UserController {
 			return res.status(401).json({
 				message: 'E-mail já existente'
 			});
-		}
+		} // Verifica se o e-mail já existe.
 
 		const values = await User.create(req.body)
+		// Registra o usuário no banco de dados.
 
-		if (values){
+		if (values) {
 			return res.status(201).json({
-				"user": values['dataValues']
+				id: values['dataValues']['id'],
+				email: values['dataValues']['email'],
+				senha: values['dataValues']['senha'],
+				data_criacao: values['dataValues']['createdAt'],
+				data_atualizacao: values['dataValues']['updatedAt']
 			})
-		}
+		} // Retorna os valores como resposta na requisição
+
+
 	};
-
-		async index(req, res) {
-		const authToken = req.headers.authorization;
-		const [, token] = authToken.split(' '); //Remove o Bearer
-
-		const checkValues = await User.findOne({
-			where: {
-				id: req.params.user_id
-			}
-		});
-
-		const checkToken = await User.findOne({
-			where: {
-				token: token,
-				id: req.params.user_id
-			}
-		});
-
-		// Verifica se o token ou usuário são divergentes
-		if (!checkToken || !checkValues) {
-			return res.status(401).json({
-				message: 'Não autorizado'
-			});
-		}
-
-		// Verifica se o token e usuários estão divergentes dos valores no DB.
-		if (!checkValues && !checkToken) {
-			return res.status(401).json({
-				message: 'Não autorizado'
-			});
-		}
-
-		if (checkValues && checkToken) {
-			var dtExpiresLogin = checkToken['dataValues']['expira_login'];
-
-			var dateNow = new Date();
-			dateNow.setHours(dateNow.getHours());
-
-			if (dtExpiresLogin < dateNow) {
-				return res.status(401).json({
-					message: 'Sessão Inválida'
-				});
-			} else {
-				return res.json({
-					user: {
-						id: checkToken['dataValues']['id'],
-						nome: checkToken['dataValues']['nome'],
-						email: checkToken['dataValues']['email'],
-						senha: checkToken['dataValues']['senha'],
-						telefone: checkToken['dataValues']['telefone'],
-						data_criacao: checkToken['dataValues']['createdAt'],
-						data_atualizacao: checkToken['dataValues']['updatedAt'],
-						ultimo_login: checkToken['dataValues']['ultimo_login']
-					}
-				});
-			}
-		}
-
-	}
 
 }
 
