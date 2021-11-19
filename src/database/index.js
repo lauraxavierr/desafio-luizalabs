324efd86547ed72af91a46b6de70989e93541bf1
lauraxavierr <laurausual@gmail.com>
2021-11-19 18:16:12 -0300

    feat: alterando estrutura de arquivo no banco de dados.

---
+++ src/database/index.js
@@ -0,0 +1,23 @@
+import Sequelize from "sequelize";
+import Users from '../app/models/Users'
+import databaseconfig from '../config/database';
+
+const models = [ Users ]
+
+class Database {
+   constructor() {
+    this.init();
+   }
+
+
+   init() {
+     this.connection = new Sequelize(databaseconfig);
+
+     models
+     .map(model => model.init(this.connection))
+     .map(model => model.associate && model.associate(this.connection.models))
+
+   }
+}
+
+export default new Database();
